af182c9e0147fa07ea66e7c19ecb3580f549a779
Ritik <ritikraj2699@outlook.com>
2023-10-30 13:12:18 +0530

    created signup view

--- autheticator/core/views.py
+++ autheticator/core/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth.models import User
 
 def homePage(request):
     return render(request,'home.html')
@@ -10,4 +10,14 @@ def loginPage(request):
 
 
 def signupPage(request):
-    return render(request,'signup.html')
+    if request.method=='POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        
+        userNew = User.objects.create_user(username,email,password1)
+        userNew.save()
+        return HttpResponse("User creted")
+    else:
+        return render(request,'signup.html')
